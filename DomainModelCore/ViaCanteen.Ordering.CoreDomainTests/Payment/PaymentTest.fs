39c4c73cdc5cb7c1618f4ce774b177cbadaff8ee
fonCki <alfonsoridao@gmail.com>
2024-03-09 06:52:53 +0100

    (sprint-2): payment and order
    Add Order and Payment modules

---
+++ DomainModelCore/ViaCanteen.Ordering.CoreDomainTests/Payment/PaymentTest.fs
@@ -0,0 +1,23 @@
+module PaymentTests
+
+open NUnit.Framework
+open System
+open CreditCard
+open MobilePay
+open ViaCard
+open Payment
+
+[<Test>]
+let ``Test CreditCard Payment Execution``() =
+    let creditCard = new CreditCard("1234-5678-9101-1121", DateTime(2024, 12, 31), 100.0)
+    Assert.DoesNotThrow(fun () -> creditCard.ExecutePayment())
+
+[<Test>]
+let ``Test MobilePay Payment Execution``() =
+    let mobilePay = new MobilePay("0800123456", 50.0)
+    Assert.DoesNotThrow(fun () -> mobilePay.ExecutePayment())
+
+[<Test>]
+let ``Test ViaCard Payment Execution``() =
+    let viaCard = new ViaCard("1111-2222-3333-4444", 75.0)
+    Assert.DoesNotThrow(fun () -> viaCard.ExecutePayment())
